3023cb0a556f547bd705098243cdaef21cd946ba
michael <yaomichael60@gmail.com>
2025-03-05 11:06:17 +0000

    Ma Fonction Register

--- user-management/fonctions.py
+++ user-management/fonctions.py
@@ -1,6 +1,13 @@
 
 
 def register(email: str, password: str):
-    with open(file_name, "a") as f:
-        f.write(f"\n Email: {email} \n Password: {password}:USER")
-        print("Inscription réussie !")
+    with open(file_name, "r+") as f:
+            data = json.load(f)
+            data["users"].append({"email": email, "password": password, "role": "USER"})
+            json.dump(data, f, indent=4)
+    else:
+        with open(file_name, "w") as f:
+            data = {"users": [{"email": email, "password": password, "role": "USER"}]}
+            json.dump(data, f, indent=4)
+    
+    print("Inscription réussie !")
